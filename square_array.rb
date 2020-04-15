cced239262d393c6f10d5755d7cee3307c335fe5
Osha G <ogroetz@gmail.com>
2020-04-15 21:12:31 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
-  number_array = []
-  number_array.each do |number|
+  new_array = []
+  array.each do |number|
     puts "{number} * {number}"
   end
   return new_array.sort!
